d82e0e15672476641f1722ee7c5127a0f338f0f7
apochukmaksym-a11y <apochuk.maksym@e-u.edu.ua>
2026-04-03 16:22:20 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,9 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
-}
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
@@ -24,5 +15,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "My Application"
 include(":app")
-include(":myapplication")
-include(":exampleunittest")
